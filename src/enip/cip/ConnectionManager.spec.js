e9f40890599b4de4fbbd14330dbe0dec75525b80
john <hirokiht@gmail.com>
2018-08-31 23:20:58 +0800

    Added LargeForwardOpen test

--- src/enip/cip/ConnectionManager.spec.js
+++ src/enip/cip/ConnectionManager.spec.js
@@ -43,4 +43,16 @@ describe('Connection Manager', () => {
     expect(ForwardOpen.build(200000, toCon, 511, toCon, 200, 0x4321, transport.direction.SERVER|transport.class[3], asmPath, 2048))
       .toMatchSnapshot()
   })
+
+  describe('LargeForwardOpen', () => {
+    let asmPath = Buffer.from('2004246630643065')
+    let otCon = connection.size.FIXED | connection.priority.LOW | connection.type.P2P
+    let toCon = connection.size.VARIABLE | connection.priority.HIGH | connection.type.MULTICAST
+    expect(LargeForwardOpen.build(100000, otCon, 555, toCon, 500, 0x1234, transport.direction.CLIENT|transport.class[0], paths.MESSAGE_ROUTER, 1024))
+      .toMatchSnapshot()
+    otCon = connection.size.VARIABLE | connection.priority.SCHEDULED | connection.type.MUTLICAST
+    toCon = connection.size.FIXED | connection.priority.URGENT | connection.type.P2P
+    expect(LargeForwardOpen.build(200000, toCon, 511, toCon, 1200, 0x4321, transport.direction.SERVER|transport.class[2], asmPath, 2048))
+      .toMatchSnapshot()
+  })
 })
